200257a9de8fa52ad2a23ad86555558b7a12a826
unclezap <zeb.blackwell@gmail.com>
2020-11-15 22:20:43 -0800

    asdfa

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,19 @@ Rails.application.routes.draw do
   # resources :pages, only: [:index, :show, :new, :edit, :update, :new, :destroy, :create]
   resources :users, only: [:index, :show, :new, :update, :new, :create]
   resources :galleries, only: [:index, :show, :edit, :update, :new, :create]
+  resources :walls, only: [:show]
+  resources :shelves, only: [:show]
+  resources :books, only: [:show]
+  resources :pages, only: [:show, :index]
   
   post '/galleries/:id/', to: "galleries#move"
+  get '/walls/:id/', to: "walls#splash", as: "wall/splash"
+  post '/walls/:id', to: "walls#show"
+  get '/shelves/:id', to: "shelves#splash", as: "shelf/splash"
+  post '/shelves/:id', to: "shelves#show"
+  get '/books/:id', to: "books#splash", as: "book/splash"
+  post '/books/:id', to: "books#show"
+  get '/pages/:id', to: "pages#splash", as: "page/splash"
 
   get '/login', to: "sessions#new", as: "login"
   post '/login', to: "sessions#create"
